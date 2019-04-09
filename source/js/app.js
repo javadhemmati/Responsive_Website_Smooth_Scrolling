7ebe5ba1c0563e819d3c92ca8f75c079bdf69363
javadhemmati <javadhemmati192@gmail.com>
2019-04-09 17:34:21 +0430

    Complete the smooth scrolling site responsive
    Adding photos to the project
    Complete the site template
    fixed the site style responsive
    Write jQuery code for smooth scrolling

--- source/js/app.js
+++ source/js/app.js
@@ -10,6 +10,10 @@ window.sr = ScrollReveal();
 // });
 
 
+
+// menu 
+
+
 $(function(){
     menu = $('nav ul');
 
@@ -34,3 +38,42 @@ $(function(){
     
     $('.open-menu').height($(window).height());
 })
+
+
+// menu 
+
+
+
+// smooth scrolling 
+
+
+$('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event){
+            if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000 , function(){
+                        var $target = $(target);
+                        $target.focus();
+                        if($target.is(":focus")){
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                } /*  if (target.length) */
+            } /*  if (location.pathname) */
+        }); /* .click(function(event) */
+
+
+
+// smooth scrolling  
+
